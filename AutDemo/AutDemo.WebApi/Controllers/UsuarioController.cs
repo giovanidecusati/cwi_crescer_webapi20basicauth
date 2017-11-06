3500337cd8937ac779c2c4ad6fad89f92df713de
Giovani Decusati <giovani@cwi.com.br>
2017-11-06 10:05:29 -0200

    feat(webapi):creating basic authentication and authorization

---
+++ AutDemo/AutDemo.WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,80 @@
+﻿using AutDemo.Dominio.Entidades;
+using AutDemo.Infraestrutura.Repositorios;
+using AutDemo.WebApi.Models;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AutDemo.WebApi.Controllers
+{
+    // Permite usuário não autenticados acessarem a controller
+    [AllowAnonymous]
+    [RoutePrefix("api/acessos")]
+    public class UsuarioController : ControllerBasica
+    {
+        readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController()
+        {
+            _usuarioRepositorio = new UsuarioRepositorio();
+        }
+
+        [HttpPost, Route("registro")]
+        public HttpResponseMessage Registrar([FromBody]RegistrarUsuarioModel model)
+        {
+            if (_usuarioRepositorio.Obter(model.Email) == null)
+            {
+                var usuario = new Usuario(model.Nome, model.Email, model.Senha);
+
+                if (usuario.Validar())
+                {
+                    _usuarioRepositorio.Criar(usuario);
+                }
+                else
+                {
+                    return ResponderErro(usuario.Mensagens);
+                }
+            }
+            else
+            {
+                return ResponderErro("Usuário já existe.");
+            }
+
+            return ResponderOK();
+        }
+
+        [HttpPost, Route("resetasenha")]
+        public HttpResponseMessage ResetarSenha(string email)
+        {
+            var usuario = _usuarioRepositorio.Obter(email);
+            if (usuario == null)
+                return ResponderErro(new string[] { "Usuário não encontrado." });
+
+            var novaSenha = usuario.ResetarSenha();
+
+            if (usuario.Validar())
+            {
+                _usuarioRepositorio.Alterar(usuario);
+                // EmailService.Enviar(usuario.Email, "Crescer 2017-1", $"Olá! sua senha foi alterada para: {novaSenha}");
+            }
+            else
+                return ResponderErro(usuario.Mensagens);
+
+            return ResponderOK();
+        }
+
+        // Exige que o usuário se autentique
+        [BasicAuthorization]
+        [HttpGet, Route("usuariologado")]
+        public HttpResponseMessage Obter()
+        {
+            // só pode obter as informações do usuário corrente (logado, autenticado)
+            //  Thread.CurrentPrincipal.Identity.Name
+            var usuario = _usuarioRepositorio.Obter(User.Identity.Name);
+
+            if (usuario == null)
+                return ResponderErro("Usuário não encontrado.");
+
+            return ResponderOK(new { usuario.Nome, usuario.Permissoes, usuario.Email });
+        }
+    }
+}
---
+++ AutDemo/AutDemo.WebApi/Models/RegistrarUsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AutDemo.WebApi.Models
+{
+    public class RegistrarUsuarioModel
+    {
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
